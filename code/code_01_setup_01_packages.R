8f28572e37cbec8f26e6f72c0105e628ae63c6d1
Joao Acharán Riffo <joaoacharan@gmail.com>
2020-06-08 11:34:53 -0400

    Instalar paquetes

---
+++ code/code_01_setup_01_packages.R
@@ -0,0 +1,17 @@
+#### SETUP 01 PACKAGES                                                    ####
+#### Repository Selection                                                 ####
+
+chooseCRANmirror(ind = 14)
+setRepositories(ind = 1)
+
+#### Packages                                                             ####
+
+install.packages("stringr")
+install.packages("stringi")
+install.packages("purrr")
+install.packages("tibble")
+install.packages("forcats")
+install.packages("ggplot2")
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("readr")
