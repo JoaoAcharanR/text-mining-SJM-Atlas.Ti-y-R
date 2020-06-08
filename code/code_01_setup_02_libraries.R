fabf7e2e0cc91b4bc1d79a3e617d62667b77a4b2
Joao Acharán Riffo <joaoacharan@gmail.com>
2020-06-08 11:35:57 -0400

    Cargar librerias

---
+++ code/code_01_setup_02_libraries.R
@@ -0,0 +1,12 @@
+#### SETUP 01 LIBRARIES  ####
+#### Library####
+
+library("stringr")
+library("stringi")
+library("purrr")
+library("tibble")
+library("forcats")
+library("ggplot2")
+library("tidyr")
+library("dplyr")
+library("readr")
