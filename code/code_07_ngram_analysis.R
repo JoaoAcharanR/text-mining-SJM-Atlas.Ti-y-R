277b78220f13605d13d1e375330c071fd8b8fa3c
Joao Acharán Riffo <joaoacharan@gmail.com>
2020-06-09 16:22:36 -0400

    Incorporación dde filtro a frecuencia de bigramas

--- code/code_07_ngram_analysis.R
+++ code/code_07_ngram_analysis.R
@@ -29,7 +29,7 @@ bigramas_separados <- bigramas_separados %>% filter(!palabra1 %in% c("eh","ciert
                                                     !palabra2 %in% c("eh","cierto","claudio","ejemplo","creo","ver","walmart","ir","ser","queso","viene","hoy","día","ok","decir","puede","puedo","ven","mucho","silos","bien","vamos"))
 bigramas_unidos   <- bigramas_separados %>% unite(bigramas, palabra1, palabra2, sep = " ")
 recuento_bigramas <- bigramas_separados %>% count(palabra1, palabra2, sort = T)
-grafo_culturales  <- recuento_bigramas  %>% graph_from_data_frame()
+grafo_culturales  <- recuento_bigramas  %>% filter(n >= 2) %>% graph_from_data_frame()
 plot_culturales   <- ggraph(grafo_culturales, layout = "nicely") + geom_edge_link(aes(edge_alpha = n), show.legend = FALSE, arrow = arrow(type = "closed", length = unit(3, "mm"))) + geom_node_point(color = "lightblue", size = 3) + geom_node_text(aes(label = name), vjust = 1, hjust = 1) + theme_void() + ggtitle("Obstaculizadores culturales")
 plot_culturales
 
@@ -51,7 +51,7 @@ bigramas_separados1 <- bigramas_separados1 %>% filter(!palabra1 %in% c("eh","cie
                                                     !palabra2 %in% c("eh","cierto","claudio","ejemplo","creo","ver","walmart","ir","ser","queso","viene","hoy","día","ok","decir","puede","puedo","ven","mucho","silos","bien","vamos"  ,"necesito","deberían","pueden","da","veo","tenido","toca","hacer","ojo","dice","paso","pareció","diciéndoles","decirlo","quiero","ósea","tengo","ibm","pasa","optando"))
 bigramas_unidos1   <- bigramas_separados1 %>% unite(bigramas, palabra1, palabra2, sep = " ")
 recuento_bigramas1 <- bigramas_separados1 %>% count(palabra1, palabra2, sort = T)
-grafo_institucionales  <- recuento_bigramas1  %>% graph_from_data_frame()
+grafo_institucionales  <- recuento_bigramas1  %>% filter(n >= 2) %>% graph_from_data_frame()
 plot_institucionales   <- ggraph(grafo_institucionales, layout = "nicely") + geom_edge_link(aes(edge_alpha = n), show.legend = FALSE, arrow = arrow(type = "closed", length = unit(3, "mm"))) + geom_node_point(color = "lightblue", size = 3) + geom_node_text(aes(label = name), vjust = 1, hjust = 1) + theme_void()  + ggtitle("Obstaculizadores institucionales")
 plot_institucionales
 
@@ -73,7 +73,7 @@ bigramas_separados2 <- bigramas_separados2 %>% filter(!palabra1 %in% c("eh","cie
                                                       !palabra2 %in% c("eh","cierto","claudio","ejemplo","creo","ver","walmart","ir","ser","queso","viene","hoy","día","ok","decir","puede","puedo","ven","mucho","silos","bien","vamos"  ,"necesito","deberían","pueden","da","veo","tenido","toca","hacer","ojo","dice","paso","pareció","diciéndoles","decirlo","quiero","ósea","tengo","ibm","pasa","optando"))
 bigramas_unidos2   <- bigramas_separados2 %>% unite(bigramas, palabra1, palabra2, sep = " ")
 recuento_bigramas2 <- bigramas_separados2 %>% count(palabra1, palabra2, sort = T)
-grafo_estado  <- recuento_bigramas2  %>% graph_from_data_frame()
+grafo_estado  <- recuento_bigramas2 %>% filter(n >= 2) %>% graph_from_data_frame()
 plot_estado   <- ggraph(grafo_estado, layout = "nicely") + geom_edge_link(aes(edge_alpha = n), show.legend = FALSE, arrow = arrow(type = "closed", length = unit(3, "mm"))) + geom_node_point(color = "lightblue", size = 3) + geom_node_text(aes(label = name), vjust = 1, hjust = 1) + theme_void() + ggtitle("Rol del Estado")
 plot_estado
 
@@ -95,7 +95,7 @@ bigramas_separados3 <- bigramas_separados3 %>% filter(!palabra1 %in% c("eh","cie
                                                       !palabra2 %in% c("eh","cierto","claudio","ejemplo","creo","ver","walmart","ir","ser","queso","viene","hoy","día","ok","decir","puede","puedo","ven","mucho","silos","bien","vamos","necesito","deberían","pueden","da","veo","tenido","toca","hacer","ojo","dice","paso","pareció","diciéndoles","decirlo","quiero","ósea","tengo","ibm","pasa","optando","poder","genera","pretende","punta","arenas","siento","llama","llegabas","tenía","sabía","existía","venir","seguir","traer","dar","puedan","vea","podía","va","sigue","di","dimos","dado","decía","incluye","llegan","queríamos","tomen","tienen","ende","hace","ablo"))
 bigramas_unidos3   <- bigramas_separados3 %>% unite(bigramas, palabra1, palabra2, sep = " ")
 recuento_bigramas3 <- bigramas_separados3 %>% count(palabra1, palabra2, sort = T)
-grafo_mercado  <- recuento_bigramas3  %>% graph_from_data_frame()
+grafo_mercado  <- recuento_bigramas3 %>% filter(n >=2)  %>% graph_from_data_frame()
 plot_mercado   <- ggraph(grafo_mercado, layout = "nicely") + geom_edge_link(aes(edge_alpha = n), show.legend = FALSE, arrow = arrow(type = "closed", length = unit(3, "mm"))) + geom_node_point(color = "lightblue", size = 3) + geom_node_text(aes(label = name), vjust = 1, hjust = 1) + theme_void() + ggtitle("Rol de las empresas")
 plot_mercado   
 
@@ -104,9 +104,9 @@ ggsave("plot_mercado.jpg", plot = plot_mercado, width = 10, height = 8)
 ####GRIDARRANGES####
 
 obstaculizadores <- grid.arrange(plot_culturales,plot_institucionales, ncol = 2,
-                                 top = textGrob("Grafos de bigramas",gp=gpar(fontface = "bold", fontsize = 15)))
+                                 top = textGrob("Redes de palabras",gp=gpar(fontface = "bold", fontsize = 15)))
 ggsave("obstaculizadores.jpg", plot = obstaculizadores, width = 12, height = 8)
 
 propuestas <- grid.arrange(plot_mercado, plot_estado, ncol = 2,
-                           top = textGrob("Grafos de bigramas",gp=gpar(fontface = "bold", fontsize = 15)))
+                           top = textGrob("Redes de palabras",gp=gpar(fontface = "bold", fontsize = 15)))
 ggsave("propuestas.jpg", plot = propuestas, width = 12, height = 8)
